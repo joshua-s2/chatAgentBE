d4a156b60116ca015f7ae28fa2fcf3e37ff0169a
JoshuaS2 <joshuajesutobi@gmail.com>
2025-10-04 07:26:32 +0100

    added port customization

--- main.py
+++ main.py
@@ -21,3 +21,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000)) 
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
